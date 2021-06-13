395c1a0f857545f25c839f32790999a7b276fca4
Ahmed Eid <ahmedeid.webdev@gmail.com>
2021-06-13 23:49:00 +0200

    node-sass fix -stable

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import './App.css';
 import Routes from './routes/routes'
-import { isLoaded } from 'react-redux-firebase'
+import { isLoaded  } from 'react-redux-firebase'
 import {useSelector} from 'react-redux';
 import SplashScreen from './components/SplashScreen/SplashScreen';
 
 function App() {
   const auth = useSelector((state) => state.firebase.auth)
+  const profile = useSelector((state) => state.firebase.profile)
   return (
     <>
     {
-      isLoaded(auth) ?
+      isLoaded(auth) && isLoaded(profile) ?
       <Routes /> : <SplashScreen/>
     }
 
